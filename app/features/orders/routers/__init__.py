bdbd138822cbe6664df3ce5576c3ed3a24a2a628
Софья Полянская <svpolianskaia@edu.hse.ru>
2025-12-17 23:02:18 +0300

    fix: remove broken checkout import from routers

--- app/features/orders/routers/__init__.py
+++ app/features/orders/routers/__init__.py
@@ -1,5 +1,4 @@
-from .checkout import router as checkout_router
 from .detail import router as detail_router
 from .my import router as my_router
 
-__all__ = ["checkout_router", "detail_router", "my_router"]
+__all__ = ["detail_router", "my_router"]
