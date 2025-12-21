c9faaa932f884b7f277750979686f1404d269d8a
Daria Shebalkova <dishebalkova@edu.hse.ru>
2025-12-21 15:07:28 +0300

    hz

--- app/main.py
+++ app/main.py
@@ -43,150 +43,150 @@ def create_app() -> FastAPI:
     async def login_page(request: Request, user: Optional[User] = Depends(get_optional_user)):
         return templates.TemplateResponse("auth/login.html", {"request": request, "user": user})
     
-    @app.get("/products/catalog", response_class=HTMLResponse)
-    async def products_page(request: Request, user: Optional[User] = Depends(get_optional_user)):
-        products = [
-            {
-                "id": 1,
-                "name": "Кирпич красный М-150 (250х120х65 мм)",
-                "manufacturer": "Воскресенский кирпичный завод",
-                "price": 22,
-                "unit": "штука",
-                "quantity_available": 1583,
-                "image_path": "https://static.tildacdn.com/stor6531-3932-4364-a332-623664396532/33785359.jpg"
-            },
-            {
-                "id": 2,
-                "name": "Кирпич облицовочный белый (250х120х88 мм)",
-                "manufacturer": "Борский силикатный завод",
-                "price": 30,
-                "unit": "штука",
-                "quantity_available": 847,
-                "image_path": "https://n-dom.com/upload/iblock/mye/myemvztczps2jqtcw0qn4wkpx12xb9o2.jpg"
-            },
-            {
-                "id": 3,
-                "name": "Газоблок D500 (600х300х200 мм)",
-                "manufacturer": "Bonolit",
-                "price": 123,
-                "unit": "штука",
-                "quantity_available": 478,
-                "image_path": "https://gbi-istra.ru/wp-content/uploads/2022/03/original-925x925-fit-4.jpg"
-            },
-            {
-                "id": 4,
-                "name": "Пеноблок (600х300х200 мм)",
-                "manufacturer": "Bonolit",
-                "price": 96,
-                "unit": "штука",
-                "quantity_available": 352,
-                "image_path": "https://stroy77.ru/upload/iblock/49c/49c40e590bf1aaf13bfd5c8e4ad72a07.jpg"
-            },
-            {
-                "id": 5,
-                "name": "Цемент М500 Д0 (50 кг)",
-                "manufacturer": "Цементум",
-                "price": 448,
-                "unit": "мешок",
-                "quantity_available": 124,
-                "image_path": "https://cementum.ru/upload/weimg_cache/eca/ecaae813213eb5077f1e6acb444f7351/e51fj6nw9ei032rdydow4a5rn0gqku31.webp"
-            },
-            {
-                "id": 6,
-                "name": "Штукатурка гипсовая Волма Слой (30 кг)",
-                "manufacturer": "Волма",
-                "price": 382,
-                "unit": "мешок",
-                "quantity_available": 87,
-                "image_path": "https://stroika-alfa.ru/upload/iblock/0aa/f3n7safsmf2butk175r8lfas4cw5kcsv/shtukaturka_gipsovaya_volma_sloy_30_kg_detail.jpg"
-            },
-            {
-                "id": 7,
-                "name": "Клей плиточный Ceresit CM 11 (25 кг)",
-                "manufacturer": "Ceresit",
-                "price": 565,
-                "unit": "мешок",
-                "quantity_available": 63,
-                "image_path": "https://media.obi.ru/resize/780x780/media/catalog/product/3/8/3811205_1_a348.jpg?store=moscow&image-type=small_image"
-            },
-            {
-                "id": 8,
-                "name": "Доска обрезная 40х200х6000 мм",
-                "manufacturer": "Лесопилка \"Тайга\"",
-                "price": 15240,
-                "unit": "м³",
-                "quantity_available": 8,
-                "image_path": "https://lesdok.ru/wa-data/public/shop/products/65/91/9165/images/12109/12109.970.jpg"
-            },
-            {
-                "id": 9,
-                "name": "Брус 100х100х6000 мм",
-                "manufacturer": "Лесопилка \"Тайга\"",
-                "price": 18150,
-                "unit": "м³",
-                "quantity_available": 5,
-                "image_path": "https://grandlesmarket.ru/assets/cache_image/products/534/brus-obreznoj-kamernoj-sushki_900x760_cf6.webp"
-            },
-            {
-                "id": 10,
-                "name": "Фанера ФК 1525х1525х12 мм",
-                "manufacturer": "Сыктывкарский ФЗ",
-                "price": 947,
-                "unit": "лист",
-                "quantity_available": 49,
-                "image_path": "https://ollq.ru/images/detailed/278/491b6fbd5409c9ceb9f8f296df86ac3b.jpg"
-            },
-            {
-                "id": 11,
-                "name": "Металлочерепица 0,5 мм",
-                "manufacturer": "Grand Line",
-                "price": 452,
-                "unit": "м²",
-                "quantity_available": 124,
-                "image_path": "https://micro-line.ru/images/detailed/1265/83842822_01.jpg"
-            },
-            {
-                "id": 12,
-                "name": "Утеплитель Rockwool 100 мм",
-                "manufacturer": "Rockwool",
-                "price": 1195,
-                "unit": "упаковка",
-                "quantity_available": 37,
-                "image_path": "https://avatars.mds.yandex.net/get-mpic/12140066/2a0000018eae752b1fffc8de0d589666de53/orig"
-            },
-            {
-                "id": 13,
-                "name": "Перфоратор Makita HR2470 780Вт 2,7 Дж",
-                "manufacturer": "Makita",
-                "price": 12480,
-                "unit": "штука",
-                "quantity_available": 7,
-                "image_path": "https://saray.ru/upload/iblock/b9c/t6wqmxioxurj0bs4mvpmpdu4em0vxcbq.jpg"
-            },
-            {
-                "id": 14,
-                "name": "Болгарка Интерскол 125 мм",
-                "manufacturer": "Интерскол",
-                "price": 3825,
-                "unit": "штука",
-                "quantity_available": 14,
-                "image_path": "https://static.baza.farpost.ru/v/1573884819809_bulletin"
-            },
-            {
-                "id": 15,
-                "name": "Набор инструментов 137 предметов",
-                "manufacturer": "Tolsen",
-                "price": 2490,
-                "unit": "набор",
-                "quantity_available": 17,
-                "image_path": "https://avatars.mds.yandex.net/get-goods_pic/10963530/hat68c9da3893a7fb23ee0fa6e882549f9e/orig"
-            }
-        ]
-        return templates.TemplateResponse("catalog/list.html", {
-            "request": request,
-            "products": products,
-            "user": user
-        })
+    # @app.get("/products/catalog", response_class=HTMLResponse)
+    # async def products_page(request: Request, user: Optional[User] = Depends(get_optional_user)):
+    #     products = [
+    #         {
+    #             "id": 1,
+    #             "name": "Кирпич красный М-150 (250х120х65 мм)",
+    #             "manufacturer": "Воскресенский кирпичный завод",
+    #             "price": 22,
+    #             "unit": "штука",
+    #             "quantity_available": 1583,
+    #             "image_path": "https://static.tildacdn.com/stor6531-3932-4364-a332-623664396532/33785359.jpg"
+    #         },
+    #         {
+    #             "id": 2,
+    #             "name": "Кирпич облицовочный белый (250х120х88 мм)",
+    #             "manufacturer": "Борский силикатный завод",
+    #             "price": 30,
+    #             "unit": "штука",
+    #             "quantity_available": 847,
+    #             "image_path": "https://n-dom.com/upload/iblock/mye/myemvztczps2jqtcw0qn4wkpx12xb9o2.jpg"
+    #         },
+    #         {
+    #             "id": 3,
+    #             "name": "Газоблок D500 (600х300х200 мм)",
+    #             "manufacturer": "Bonolit",
+    #             "price": 123,
+    #             "unit": "штука",
+    #             "quantity_available": 478,
+    #             "image_path": "https://gbi-istra.ru/wp-content/uploads/2022/03/original-925x925-fit-4.jpg"
+    #         },
+    #         {
+    #             "id": 4,
+    #             "name": "Пеноблок (600х300х200 мм)",
+    #             "manufacturer": "Bonolit",
+    #             "price": 96,
+    #             "unit": "штука",
+    #             "quantity_available": 352,
+    #             "image_path": "https://stroy77.ru/upload/iblock/49c/49c40e590bf1aaf13bfd5c8e4ad72a07.jpg"
+    #         },
+    #         {
+    #             "id": 5,
+    #             "name": "Цемент М500 Д0 (50 кг)",
+    #             "manufacturer": "Цементум",
+    #             "price": 448,
+    #             "unit": "мешок",
+    #             "quantity_available": 124,
+    #             "image_path": "https://cementum.ru/upload/weimg_cache/eca/ecaae813213eb5077f1e6acb444f7351/e51fj6nw9ei032rdydow4a5rn0gqku31.webp"
+    #         },
+    #         {
+    #             "id": 6,
+    #             "name": "Штукатурка гипсовая Волма Слой (30 кг)",
+    #             "manufacturer": "Волма",
+    #             "price": 382,
+    #             "unit": "мешок",
+    #             "quantity_available": 87,
+    #             "image_path": "https://stroika-alfa.ru/upload/iblock/0aa/f3n7safsmf2butk175r8lfas4cw5kcsv/shtukaturka_gipsovaya_volma_sloy_30_kg_detail.jpg"
+    #         },
+    #         {
+    #             "id": 7,
+    #             "name": "Клей плиточный Ceresit CM 11 (25 кг)",
+    #             "manufacturer": "Ceresit",
+    #             "price": 565,
+    #             "unit": "мешок",
+    #             "quantity_available": 63,
+    #             "image_path": "https://media.obi.ru/resize/780x780/media/catalog/product/3/8/3811205_1_a348.jpg?store=moscow&image-type=small_image"
+    #         },
+    #         {
+    #             "id": 8,
+    #             "name": "Доска обрезная 40х200х6000 мм",
+    #             "manufacturer": "Лесопилка \"Тайга\"",
+    #             "price": 15240,
+    #             "unit": "м³",
+    #             "quantity_available": 8,
+    #             "image_path": "https://lesdok.ru/wa-data/public/shop/products/65/91/9165/images/12109/12109.970.jpg"
+    #         },
+    #         {
+    #             "id": 9,
+    #             "name": "Брус 100х100х6000 мм",
+    #             "manufacturer": "Лесопилка \"Тайга\"",
+    #             "price": 18150,
+    #             "unit": "м³",
+    #             "quantity_available": 5,
+    #             "image_path": "https://grandlesmarket.ru/assets/cache_image/products/534/brus-obreznoj-kamernoj-sushki_900x760_cf6.webp"
+    #         },
+    #         {
+    #             "id": 10,
+    #             "name": "Фанера ФК 1525х1525х12 мм",
+    #             "manufacturer": "Сыктывкарский ФЗ",
+    #             "price": 947,
+    #             "unit": "лист",
+    #             "quantity_available": 49,
+    #             "image_path": "https://ollq.ru/images/detailed/278/491b6fbd5409c9ceb9f8f296df86ac3b.jpg"
+    #         },
+    #         {
+    #             "id": 11,
+    #             "name": "Металлочерепица 0,5 мм",
+    #             "manufacturer": "Grand Line",
+    #             "price": 452,
+    #             "unit": "м²",
+    #             "quantity_available": 124,
+    #             "image_path": "https://micro-line.ru/images/detailed/1265/83842822_01.jpg"
+    #         },
+    #         {
+    #             "id": 12,
+    #             "name": "Утеплитель Rockwool 100 мм",
+    #             "manufacturer": "Rockwool",
+    #             "price": 1195,
+    #             "unit": "упаковка",
+    #             "quantity_available": 37,
+    #             "image_path": "https://avatars.mds.yandex.net/get-mpic/12140066/2a0000018eae752b1fffc8de0d589666de53/orig"
+    #         },
+    #         {
+    #             "id": 13,
+    #             "name": "Перфоратор Makita HR2470 780Вт 2,7 Дж",
+    #             "manufacturer": "Makita",
+    #             "price": 12480,
+    #             "unit": "штука",
+    #             "quantity_available": 7,
+    #             "image_path": "https://saray.ru/upload/iblock/b9c/t6wqmxioxurj0bs4mvpmpdu4em0vxcbq.jpg"
+    #         },
+    #         {
+    #             "id": 14,
+    #             "name": "Болгарка Интерскол 125 мм",
+    #             "manufacturer": "Интерскол",
+    #             "price": 3825,
+    #             "unit": "штука",
+    #             "quantity_available": 14,
+    #             "image_path": "https://static.baza.farpost.ru/v/1573884819809_bulletin"
+    #         },
+    #         {
+    #             "id": 15,
+    #             "name": "Набор инструментов 137 предметов",
+    #             "manufacturer": "Tolsen",
+    #             "price": 2490,
+    #             "unit": "набор",
+    #             "quantity_available": 17,
+    #             "image_path": "https://avatars.mds.yandex.net/get-goods_pic/10963530/hat68c9da3893a7fb23ee0fa6e882549f9e/orig"
+    #         }
+    #     ]
+    #     return templates.TemplateResponse("catalog/list.html", {
+    #         "request": request,
+    #         "products": products,
+    #         "user": user
+    #     })
 
     @app.get("/cart", response_class=HTMLResponse, include_in_schema=False)
     async def cart_page(
